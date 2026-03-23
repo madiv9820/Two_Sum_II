a467ee9861eefa6d5ba8efa335e59f2cc62b4212
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-03-23 21:04:22 +0530

    feat(cpp): add initial C++ solution skeleton for Two Sum II 💀

---
+++ source/cpp/solution.hpp
@@ -0,0 +1,14 @@
+#define SOLUTION_HPP
+#ifdef SOLUTION_HPP
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSums(vector<int>& numbers, int target) {
+        return vector<int>();
+    }
+};
+
+#endif
